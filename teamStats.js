b0396d16edc64693682ac08e6f2471c176e3a4c9
tafsirony <onykhan95@gmail.com>
2020-07-02 21:01:57 +0600

    array push

--- teamStats.js
+++ teamStats.js
@@ -1,66 +1,64 @@
-const team ={
-    _players:[
-      {
-        firstName: 'Pablo',
-        lastName: 'Sanchez',
-        age: 11
-      },
-       {
-        firstName: 'Pete', 
-        lastName: 'Wheeler', 
-        age: 54
-      },
-       {
-        firstName: 'Messi', 
-        lastName: 'Striker', 
-        age: 31
-      }
-      ],
-    _games:[
-        {
-      opponent: 'Broncos',
+const team = {
+  _players: [
+    {
+      firstName: "Pablo",
+      lastName: "Sanchez",
+      age: 11,
+    },
+    {
+      firstName: "Pete",
+      lastName: "Wheeler",
+      age: 54,
+    },
+    {
+      firstName: "Messi",
+      lastName: "Striker",
+      age: 31,
+    },
+  ],
+  _games: [
+    {
+      opponent: "Broncos",
       teamPoints: 42,
-      opponentPoints: 27
+      opponentPoints: 27,
     },
-       {
-      opponent: 'Defence',
+    {
+      opponent: "Defence",
       teamPoints: 22,
-      opponentPoints: 17
+      opponentPoints: 17,
     },
-       {
-      opponent: 'MidField',
+    {
+      opponent: "MidField",
       teamPoints: 62,
-      opponentPoints: 37
+      opponentPoints: 37,
     },
-    
-    ],
-  
-  get players(){
-    return this._player;
+  ],
+
+  get players() {
+    return this._players;
   },
-  get games(){
+  get games() {
     return this._games;
   },
-  addPlayer(firstName,lastName,age){
+  addPlayer(firstName, lastName, age) {
     let player = {
       firstName: firstName,
-      lastName : lastName,
-      age : age
+      lastName: lastName,
+      age: age,
     };
-    this._players.push(player);
+    this.players.push(player);
   },
-  addGame(name,myteamPoints,OppPoints){
+  addGame(name, myteamPoints, OppPoints) {
     const game = {
       opponent: name,
       teamPoints: myteamPoints,
-      opponentPoints: OppPoints
+      opponentPoints: OppPoints,
     };
     this.games.push(game);
-  }
-  };
-  
-  const newPlayer = team.addPlayer("ony","khan",23);
-  console.log(team._players[3]);
-  const newGame = team.addGame('wings',78,67);
-  console.log(team.games[3]);
-  
+  },
+};
+
+const newPlayer = team.addPlayer("ony", "khan", 23);
+console.log(team._players[3]);
+const newGame = team.addGame("wings", 78, 67);
+console.log(team.games[3]);
